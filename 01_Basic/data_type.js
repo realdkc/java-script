18ac002c21b2133e3b2be7d3ad779aa66bfe289a
realdkc <realdkc31@gmail.com>
2025-01-15 08:48:33 +0000

    data type

---
+++ 01_Basic/data_type.js
@@ -0,0 +1,29 @@
+"use strict"; // trest all js code as newer version
+
+//alert("hello") // we are using node js
+
+console.log(3+3)
+
+
+
+
+
+
+let name = "real_dkc"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+// null => stand alone value
+// undefined
+// symbol =. unique
+
+
+// object
+
+//console.log(typeof "real_dkc")
+console.log(typeof null)
