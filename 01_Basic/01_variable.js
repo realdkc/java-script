449d918ceaaf6157d5763f370ed98f0f81fbeb2a
realdkc <realdkc31@gmail.com>
2025-01-15 07:26:08 +0000

    Let, const

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,14 @@
+const account_Id = 144553
+let accountEmail = "div@gamil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bangluru"
+
+console.log("accountId");
+
+console.table([account_Id, accountCity, accountPassword])
