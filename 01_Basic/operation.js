bbc9935691bbbcf4279f628ad52dcb24a98ad0fe
realdkc <realdkc31@gmail.com>
2025-01-15 15:59:41 +0000

    operations in js

---
+++ 01_Basic/operation.js
@@ -0,0 +1,14 @@
+//let value = 3
+//let negvalue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "user"
+
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+
