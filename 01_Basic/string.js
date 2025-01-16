a5f843e812e73de24f359d912312b50bbac62349
realdkc <realdkc31@gmail.com>
2025-01-16 11:18:40 +0000

    information about string

---
+++ 01_Basic/string.js
@@ -0,0 +1,19 @@
+//const name = real_dkc;
+
+//console.log("hello" + "buddy" + name);
+//console.log('Hello buddy ${name}');
+
+const gameName = new String('real_dkc')
+console.log(gameName[0]);
+//console.log(gameName);
+
+//console.log(gameName.__proto__);
+//console.log(gameName. length);
+//console.log(gameName.charAt(4));
+
+//console.log(gameName.indexOf('k'));
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-2, 4)
+console.log(anotherString);
