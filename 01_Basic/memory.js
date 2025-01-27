c0abdb8b45dc33e8e22986fa1c8f584719773547
realdkc <realdkc31@gmail.com>
2025-01-27 05:14:03 +0000

    memories

---
+++ 01_Basic/memory.js
@@ -0,0 +1,17 @@
+/*let myName = "Divyanshu kumar chauhan"
+
+let anothername = myName
+//anothername = "real_dkc"
+//console.log(anothername);*/
+
+
+
+let userOne = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "Div@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
