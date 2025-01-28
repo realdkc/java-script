43e71991db5d9f308c575ef541ec7b6480b411e7
realdkc <realdkc31@gmail.com>
2025-01-28 06:48:00 +0000

    Date&Time

---
+++ 01_Basic/Date.js
@@ -0,0 +1,24 @@
+/*let myDate = new Date()
+console.log(myDate. toString());
+console.log(myDate. toDateString());
+console.log(myDate. toLocaleString());
+console.log(typeof myDate);*/
+
+
+
+let myCreatedDate = new Date("01-28-2025")
+//console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+/*console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));*/
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('daefault', {
+    weekday:"long"
+})
