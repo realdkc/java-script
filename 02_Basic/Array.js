18fe8555fff01048b62fbaf6da13daf0f5badcbd
realdkc <realdkc31@gmail.com>
2025-01-28 09:56:44 +0000

    Array & Methods

---
+++ 02_Basic/Array.js
@@ -0,0 +1,20 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Shaktiman", "iron man", "Nagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[5]);
+
+
+//Array methods
+
+
+/*myArr.push(6)
+myArr.push(7)
+myArr.pop()*/
+//myArr.unshift(9)
+
+//myArr.shift(9)
+console.log(myArr.includes(0));
+console.log(myArr.indexOf(2));
+
+console.log(myArr);
