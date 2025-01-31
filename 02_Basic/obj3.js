f104f6d823754784d63fb59b6ac9530c0bd93c19
realdkc <realdkc31@gmail.com>
2025-01-31 11:37:49 +0000

    object 3

---
+++ 02_Basic/obj3.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename:"js",
+    price:"999",
+    courseInstructor:"Angela"
+}
+
+
+const {courseInstructor: instructor} = course
+/*console.log(courseInstructor);*/
+console.log(instructor);
+
+
+/*const navbar = () => {
+
+
+}
+navbar(company = "real_dkc")*/
+
+/*{
+    "name": "hitesh"
+    "coursename": "js",
+    "price":"free"
+}*/
+[
+    
+    {},
+    {},
+    {},
+]
